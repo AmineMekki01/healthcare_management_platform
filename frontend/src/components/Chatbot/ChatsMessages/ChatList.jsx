7f5a7da163974906e2632517a7ae64f4a330e9f4
AmineMekki01 <amine.mekki.contact@gmail.com>
2024-08-25 18:34:23 +0200

    Fixing how chats are displayed in the ChatList

--- frontend/src/components/Chatbot/ChatsMessages/ChatList.jsx
+++ frontend/src/components/Chatbot/ChatsMessages/ChatList.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import moment from 'moment';
 
 const Chat = styled.button`
   padding: 10px;
   color: #fff;
   font-size: 17px;
-  background-color: ${props => props.isSelected ? '#607ba0' : '#465B7A'}; // Default color and selected color
+  background-color: ${props => props.isSelected ? '#607ba0' : '#465B7A'};
   &:hover {
-    background-color: #607ba0; // Color on hover
+    background-color: #607ba0;
   }
-  transition: background-color 0.3s; // Smooth transition for background color change
+  transition: background-color 0.3s;
 `;
 
 const ChatsList = styled.div`
@@ -17,25 +18,68 @@ const ChatsList = styled.div`
   flex-direction: column;
   padding: 10px;
   overflow-y: auto;
-  // hide scrollbar
   ::-webkit-scrollbar {
     display: none;
   }
-  -ms-overflow-style: none;  // IE and Edge
-  scrollbar-width: none;  // Firefox
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 
 `;
 
+const MessageAndTime = styled.p`
+    display: flex;
+    justify-content: space-between;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin: 0;
+    color: lightgray;
+    width: 100%;  
+
+`;
+
+const MessageContent = styled.span`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`;
+
+const MessageTime = styled.span`
+    margin-left: 10px; 
+    white-space: nowrap;
+`;
+
 const ChatList = ({ chats, onSelectChat, selectedChatId }) => {
+  const formatMessageDate = (dateString) => {
+    if (!dateString) {
+        return 'just now';
+    }
+    return moment(dateString).calendar(null, {
+        sameDay: 'LT',
+        nextDay: '[Tomorrow at] LT',
+        nextWeek: 'dddd [at] LT',
+        lastDay: '[Yesterday at] LT',
+        lastWeek: '[Last] dddd [at] LT',
+        sameElse: 'L'
+    });
+};
+
+
+  const sortedChat = [...chats].sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
+
   return (
     <ChatsList>
-      {chats.map((chat) => (
+      {sortedChat.map((chat) => (
         <Chat
           key={chat.id}
           onClick={() => onSelectChat(chat.id)}
           isSelected={selectedChatId === chat.id} 
         >
-          {chat.title}
+          <MessageAndTime>
+            <MessageContent>{chat.title}</MessageContent>
+            <MessageTime>{formatMessageDate(chat.created_at)}</MessageTime>
+
+        </MessageAndTime>
         </Chat>
       ))}
     </ChatsList>
